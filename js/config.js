e3f66de9679e7c4164acba6cfd2ccc2adf202a1e
Neil Raikar <79545222+neilraikar@users.noreply.github.com>
2025-04-17 10:14:04 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_KvCYdvnze', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3q3o4fj7324qbt5ku2n4ngchgd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_qsb2tSJsK', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '49mt4i24dth2mircn4v2acvhq7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
